d7cc8517eb9467af628704d362eb8f0c8cf62753
FomichovAleks <webmastergoogl@gmail.com>
2019-04-20 18:46:06 +0200

    Новые файлы

--- Lesson6/templates/menu.php
+++ Lesson6/templates/menu.php
@@ -3,6 +3,8 @@
         <ul>
             <li><a href="index.php" class="active">Главная</a></li>
             <li><a href="catalog.php">Каталог</a></li>
+            <li><a href="guestbook.php">Отзывы</a></li>
+            <li><a href="gallery.php">Фото</a></li>
             
         </ul>
     </div>
